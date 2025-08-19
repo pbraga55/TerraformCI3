48bfec5c7fbd783ba4e80ac8950a5245d45d703e
Paulo Braga <pbraga55@gmail.com>
2025-08-18 17:30:52 -0700

    dsada

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_resource_group" "rg" {
 
 
 resource "azurerm_storage_account" "storageaccount" {
-  name                     = "stg${random_string.uniquestring.result}"
+  name                     = "stg32729832jn23"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"

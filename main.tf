2544567d6f4a76acd89b7af6bc00ac231f656878
Paulo Braga <pbraga55@gmail.com>
2025-08-20 20:43:23 -0700

    destroy

--- main.tf
+++ main.tf
@@ -38,22 +38,14 @@ resource "azurerm_resource_group" "rg" {
 #  account_replication_type = "LRS"
 #}
 
-module "finaltestmodule" {
-  source  = "app.terraform.io/NonationITServices/finaltestmodule/azurerm"
-  version = "1.0.0"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  name                = "pb87sdf89mn83erj"
-  environment          = "DEV"
+#module "finaltestmodule" {
+ # source  = "app.terraform.io/NonationITServices/finaltestmodule/azurerm"
+ # version = "1.0.0"
+ # resource_group_name = azurerm_resource_group.rg.name
+ # location            = azurerm_resource_group.rg.location
+  #name                = "pb87sdf89mn83erj"
+ # environment          = "DEV"
   # insert required variables here
-}
+#}
 
-module "finaltestmodule" {
-  source  = "app.terraform.io/NonationITServices/finaltestmodule/azurerm"
-  version = "1.0.0"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  name                = "sdf89mn83er34434dsj"
-  environment          = "Production"
-  # insert required variables here
-}
+
